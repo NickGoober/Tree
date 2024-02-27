3bce2c9704af91cbf88f7c7b69e6e13514daf522
Gopanik <Gopanik@DESKTOP-OUKEP5E>
2024-02-27 15:49:45 +0000

    Add project files.

---
+++ Tree/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+
+            tree.Add(10);
+            tree.Add(15);
+            tree.Add(23);
+            tree.Add(3);
+            tree.Add(6);
+            Console.WriteLine(tree.root.data);
+            tree.InOrder();
+        }
+    }
+}
---
+++ Tree/Tree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable enable
+namespace Tree
+{
+    internal class Tree
+    {
+        public Node? root = null;
+
+        public Tree()
+        {
+            root = null;
+        }
+
+        public void Add( int value)
+        {
+            
+            if(root == null)
+            {
+                Node newnode = new Node(value);
+                root = newnode;
+                Console.WriteLine(newnode.data);
+            }
+            else
+            {
+                AddRec(root, value);
+            }
+        }
+        private Node AddRec(Node selected, int value)
+        {
+
+            if (selected == null) 
+            {
+                Node newnode = new Node(value);
+                selected = newnode;
+            } 
+            else
+            {
+                if (value > selected.data)
+                {
+                    selected =  AddRec(selected.right, value);
+                }
+                if (value < selected.data)
+                {
+                    selected =  AddRec(selected.left, value);
+                }
+            }
+
+            return selected;
+        }
+
+        public void InOrder()
+        {
+            InOrderRec(root);
+        }
+
+        public void InOrderRec(Node selected)
+        {
+            if(selected != null)
+            {
+                InOrderRec(selected.left);
+                Console.WriteLine(selected.data);
+                InOrderRec(selected.right);
+            }
+        }
+    }
+}
